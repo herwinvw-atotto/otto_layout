26ca23757df336f56af0fc2ffe0f950cb3634e1b
hewelber <herwin.vanwelbergen@otto.de>
2023-08-20 22:27:03 +0200

    fixed font path for usage as package

--- src/otto_layout/fonts.py
+++ src/otto_layout/fonts.py
@@ -2,9 +2,10 @@ from matplotlib import font_manager
 import os
 
 def add_fonts():
-  font_dir = f"{os.path.dirname(__file__)}/../../fonts"
-  print(font_dir)
-  font_files = font_manager.findSystemFonts(fontpaths=[font_dir])
+  local_font_dir = f"{os.path.dirname(__file__)}/../../fonts"
+  package_font_dir = f"{os.path.dirname(__file__)}/../../../../src/otto_layout/fonts"
+  print(package_font_dir)
+  font_files = font_manager.findSystemFonts(fontpaths=[local_font_dir, package_font_dir])
   for font_file in font_files:
     print(font_file)
     font_manager.fontManager.addfont(font_file)
