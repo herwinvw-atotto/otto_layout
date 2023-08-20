1b9056c7fb1a47e6f250a50f8e39fa56eaf475dd
hewelber <herwin.vanwelbergen@otto.de>
2023-08-20 22:30:09 +0200

    removed debug prints

--- src/otto_layout/fonts.py
+++ src/otto_layout/fonts.py
@@ -4,8 +4,6 @@ import os
 def add_fonts():
   local_font_dir = f"{os.path.dirname(__file__)}/../../fonts"
   package_font_dir = f"{os.path.dirname(__file__)}/../../../../src/otto_layout/fonts"
-  print(package_font_dir)
   font_files = font_manager.findSystemFonts(fontpaths=[local_font_dir, package_font_dir])
   for font_file in font_files:
-    print(font_file)
     font_manager.fontManager.addfont(font_file)
